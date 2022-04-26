0a94348ea45fc622f8dd5c6eb4d02070d001fe31
Shyby Mallanveliyil <shybyhamza@gmail.com>
2022-04-26 12:56:35 -0500

    Sending email and Social sharing

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Text, View, ScrollView, FlatList, 
-    Modal, Button, StyleSheet, Alert, PanResponder } from "react-native";
+    Modal, Button, StyleSheet, Alert, PanResponder, Share } from "react-native";
 import { Card, Icon, Rating, Input } from "react-native-elements";
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -60,7 +60,17 @@ function RenderCampsite(props) {
             }
             return true;
         }
-    })
+    });
+
+    const shareCampsite = (title, message, url) => {
+        Share.share({
+            title,
+            message: `${title}: ${message} ${url}`,
+            url
+        }, {
+            dialogTitle: 'Share' + title
+        });
+    };
 
     if (campsite) {
         return (
@@ -95,6 +105,15 @@ function RenderCampsite(props) {
                             reverse
                             onPress={() => props.onShowModal()}
                             
+                        />
+                        <Icon
+                            name={'share'}
+                            type='font-awesome'
+                            color='#5637DD'
+                            raised
+                            reverse
+                            onPress={() => shareCampsite(campsite.name, campsite.description, 
+                                baseUrl + campsite.image)}
                         />
                     </View>
                 </Card>
